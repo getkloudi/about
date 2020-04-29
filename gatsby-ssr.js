971b38a23ecb7f914cacb3498a5ccf3b327cad57
Nitish Mehrotra <nitish@kloudi.tech>
2020-04-29 13:55:39 +0530

    Added chatwoot to the website.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,32 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
 
-// You can delete this file if you're not using it
+export const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+  // Determine whether widget should be displayed.
+  if (process.env.NODE_ENV !== `production`) {
+    return null;
+  }
+
+  const baseUrl = 'https://app.chatwoot.com';
+  const websiteToken = process.env.CHATWOOT_WEBSITE_TOKEN;
+
+  return setPostBodyComponents([
+    <script
+      key={`gatsby-plugin-chatwoot`}
+      dangerouslySetInnerHTML={{
+        __html: `
+                    (function(d,t) {
+                    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                    g.src="${baseUrl}/packs/js/sdk.js";
+                    s.parentNode.insertBefore(g,s);
+                    g.onload=function(){
+                      window.chatwootSDK.run({
+                        websiteToken: "${websiteToken}",
+                        baseUrl: "${baseUrl}"
+                      })
+                    }
+                    })(document,"script");
+                `.trim(),
+      }}
+    />,
+  ]);
+};
