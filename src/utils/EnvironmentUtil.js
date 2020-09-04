b51e13801b3e3e05cb26616401f86a058a805a5e
Nitish Mehrotra <nitish@kloudi.tech>
2020-09-04 21:21:28 +0530

    Added an util function to get the os of the browser

--- src/utils/EnvironmentUtil.js
+++ src/utils/EnvironmentUtil.js
@@ -2,4 +2,28 @@ export default class EnvironmentUtil {
   static getApiBaseUrl() {
     return process.env.GATSBY_API_URL;
   }
+
+  static getOS() {
+    var os;
+    if (typeof window !== `undefined`) {
+      var userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+        windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+        iosPlatforms = ["iPhone", "iPad", "iPod"];
+
+      if (macosPlatforms.indexOf(platform) !== -1) {
+        os = "Mac OS";
+      } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = "iOS";
+      } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = "Windows";
+      } else if (/Android/.test(userAgent)) {
+        os = "Android";
+      } else if (!os && /Linux/.test(platform)) {
+        os = "Linux";
+      }
+    }
+    return os;
+  }
 }
